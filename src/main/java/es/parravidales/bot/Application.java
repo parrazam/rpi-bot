03effd23ca5bc19ce44c61afe13b59592efae487
Parra <parra.vidales+github@gmail.com>
2018-09-08 20:31:55 +0200

    Añadido comentario de soporte a perfiles

--- src/main/java/es/parravidales/bot/Application.java
+++ src/main/java/es/parravidales/bot/Application.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 
+/**
+ * Main class for bot
+ * To run this class, remember fill USER_ID and TOKEN_ID in application.yml
+ * or define other profile and run with -Dspring.profiles.active=your_profile
+ */
 @SpringBootApplication
 public class Application {
 
